52a1aaa05c6aa7b1d0d95969cace28c6cd88e74b
Jesus Morales <jesusam@umich.edu>
2013-05-15 09:31:46 -0400

    Initial Commit

---
+++ CompanyHours/CHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CHAppDelegate.h
+//  CompanyHours
+//
+//  Created by Jesus Morales on 5/15/13.
+//  Copyright (c) 2013 Jesus Morales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CompanyHours/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CompanyHours
+//
+//  Created by Jesus Morales on 5/15/13.
+//  Copyright (c) 2013 Jesus Morales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CHAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CHAppDelegate class]));
+    }
+}
